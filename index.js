a123ae574f28cef41527b86e6a2e0fa314de2536
Sunjida-Srijoni <sunjidasrijoni@gmail.com>
2024-02-18 11:53:53 +0600

    clock is created

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const saveButton = document.querySelector(".btn");
 console.log(saveButton);
-const sampleText = document.getElementsByClassName("text");
+const sampleText = document.querySelector(".text");
 
 saveButton.addEventListener("click", createClock);
 
 function createClock(){
     console.log("inside create clock");
+
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let time = hours + ":" + minutes + ":" + seconds;
+
+    console.log(time);
+    sampleText.textContent = time;
+
+    setInterval(createClock, 1000)
 }
