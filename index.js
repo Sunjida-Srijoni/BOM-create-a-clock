6b5b9cd4938a86a98702bf954c8bfbee82fd3336
Sunjida-Srijoni <sunjidasrijoni@gmail.com>
2024-02-18 11:57:28 +0600

    clock with double digit at last is created

--- index.js
+++ index.js
@@ -11,10 +11,23 @@ function createClock(){
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
+
+    minutes = formatTime(minutes);
+    seconds = formatTime(seconds);
+
+
+
     let time = hours + ":" + minutes + ":" + seconds;
 
     console.log(time);
     sampleText.textContent = time;
 
     setInterval(createClock, 1000)
+}
+
+function formatTime(value){
+    if(value < 10){
+        value = "0" + value;
+    }
+    return value;
 }
